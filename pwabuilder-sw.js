c2589d41117df7c0c517e03498d7cbdb4b28297e
ravicoder123-star <ravishaw9689@gmail.com>
2025-09-10 01:39:22 +0530

    Update pwabuilder-sw.js

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -4,8 +4,8 @@ const CORE_ASSETS = [
   '/index.html',
   '/styles.css',     // add your file names
   '/game.js',        // add your file names
-  '/icons/icon-192.png',
-  '/icons/icon-512.png'
+  '/icon-192.png',
+  '/icon-512.png'
 ];
 
 self.addEventListener('install', event => {
