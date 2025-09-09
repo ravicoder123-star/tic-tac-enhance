1110502617475c9ca45c0dc3f2367be70a33056f
ravicoder123-star <ravishaw9689@gmail.com>
2025-09-09 22:59:58 +0530

    Create pwabuilder-sw.js

---
+++ pwabuilder-sw.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'tic-tac-enhance-v1';
+const CORE_ASSETS = [
+  '/',
+  '/index.html',
+  '/styles.css',     // add your file names
+  '/game.js',        // add your file names
+  '/icons/icon-192.png',
+  '/icons/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(CORE_ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request))
+  );
+});
